a6c2f8105d2a57033f548e1062d40d47bbab68f7
Yicong-Huang <hyc541978023@gmail.com>
2020-04-03 09:28:17 -0700

    fixed loop for one-time query

--- asterixdb_pump.py
+++ asterixdb_pump.py
@@ -6,6 +6,7 @@ import re
 import shelve
 import urllib.parse
 from datetime import datetime, timedelta
+from socket import socket
 from time import sleep
 from typing import List
 
@@ -36,50 +37,73 @@ class PumpManager:
         self.update_time = parse(PUMP_CONFIG_PATH)['selection_query']['start_time']
         self.update_count = 0
         self.total_count = 0
-
         self.last_update_count = 0
         self.last_update_time = self.update_time
 
     def pump(self):
+        datefmt = '%Y-%m-%dT%H:%M:%SZ'
+
         try:
             config = parse(PUMP_CONFIG_PATH)
-            dest = config['asterixdb_dest']
-            src = config['asterixdb_src']
-            query = config['selection_query']
+            src = config.get('asterixdb_src')
+            dest = config.get('asterixdb_dest')
+            query = config.get('selection_query')
+            general = config.get('general')
 
-            if eval(query['restart']):
-                self.update_time = query['start_time']
-                logging.info(f"RESTARTING from {self.update_time}")
+            if eval(query.get('incremental')):
 
-            self.last_update_time = self.update_time
-            self.last_update_count = self.update_count
-            end_time = query['end_time']
-            if end_time == "now":
-                end_time = (datetime.utcnow()).strftime('%Y-%m-%dT%H:%M:%SZ')
+                if eval(query.get('restart')):
+                    self.update_time = query.get('start_time')
+                    logging.info(f"RESTARTING from {self.update_time}")
 
-            if config['general'].get('delay'):
-                sleep(int(config['general'].get('delay')))
-            start_time = self.update_time
-            while datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%SZ') < datetime.strptime(end_time,
-                                                                                          '%Y-%m-%dT%H:%M:%SZ'):
-                period_end_time = (datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%SZ') + timedelta(days=1)).strftime(
-                    '%Y-%m-%dT%H:%M:%SZ')
-                if datetime.strptime(period_end_time, '%Y-%m-%dT%H:%M:%SZ') > datetime.strptime(
-                        end_time, '%Y-%m-%dT%H:%M:%SZ'):
-                    period_end_time = end_time
-                data = self.pull_from_asterixdb(scheme=src.get('scheme'), host=src.get('host'), port=src.get('port'),
-                                                start_time=start_time, end_time=period_end_time)
-                if eval(config['general']['persist_to_disk']):
-                    self.persist_to_disk(data, config['general']['persistence_path'])
-                self.push_to_asterixdb(data, dest.get('host'), dest.get('port'))
                 self.last_update_time = self.update_time
-                self.update_time = period_end_time
                 self.last_update_count = self.update_count
-                self.update_count += len(data)
-                logging.info(self)
-                start_time = self.update_time
+                end_time = query.get('end_time')
+                if end_time == "now":
+                    end_time = (datetime.utcnow()).strftime(datefmt)
+
+                self._pre_run_delay(general)
+                self._time_looped_pump(src, dest, general, datefmt, end_time)
+            else:
+                self._one_time_pump(src, dest, config)
+
         except Exception as e:
             logging.error(e)
+
+    def _one_time_pump(self, src, dest, general):
+        data = self.pull_from_asterixdb(scheme=src.get('scheme'), host=src.get('host'), port=src.get('port'))
+        if eval(general.get('persist_to_disk')):
+            self.persist_to_disk(data, general.get('persistence_path'))
+        self.push_to_asterixdb(data, dest.get('host'), dest.get('port'))
+        self.last_update_count = self.update_count
+        self.update_count += len(data)
+        logging.info(self)
+
+    def _time_looped_pump(self, src, dest, general, datefmt, end_time):
+        period_start_time = self.update_time
+        while datetime.strptime(period_start_time, datefmt) < datetime.strptime(end_time, datefmt):
+            period_end_time = (datetime.strptime(period_start_time, datefmt) + timedelta(days=1)).strftime(
+                datefmt)
+            if datetime.strptime(period_end_time, datefmt) > datetime.strptime(end_time, datefmt):
+                period_end_time = end_time
+            data = self.pull_from_asterixdb(scheme=src.get('scheme'), host=src.get('host'), port=src.get('port'),
+                                            start_time=period_start_time, end_time=period_end_time)
+            if eval(general.get('persist_to_disk')):
+                self.persist_to_disk(data, general.get('persistence_path'))
+            self.push_to_asterixdb(data, dest.get('host'), dest.get('port'))
+            self.last_update_time = self.update_time
+            self.update_time = period_end_time
+            self.last_update_count = self.update_count
+            self.update_count += len(data)
+            logging.info(self)
+            period_start_time = self.update_time
+
+    @staticmethod
+    def _pre_run_delay(general):
+        delay = general.get('delay')
+        if delay:
+            logging.info(f"waiting for {delay} seconds")
+            sleep(int(delay))
 
     @staticmethod
     def pull_from_asterixdb(scheme: str, host: str, port: int, start_time=None, end_time=None) -> List[str]:
@@ -87,9 +111,7 @@ class PumpManager:
         asterixdb_url = f"{scheme}://{host}:{port}/query/service"
         assert host and port and validate_url(
             asterixdb_url), f"The provided asterixdb url is not correct: {asterixdb_url}"
-
-        headers = {"Content-Type": "application/x-www-form-urlencoded"}
-
+        logging.info("--------------------------------------------------------")
         incremental = eval(config['selection_query']['incremental'])
         logging.info(f"RUNNING in {'incremental' if incremental else 'one time'} mode")
 
@@ -100,9 +122,11 @@ class PumpManager:
             assert re.search(re.compile(r'.*(WHERE).*', re.IGNORECASE), sql), "NO WHERE clause found"
             assert '<time-window>' in sql, "NO <time-window> found"
             sql = sql.replace("<time-window>",
-                              f" AND t.create_at >= datetime({repr(start_time)}) AND t.create_at < datetime({repr(end_time)})")
+                              f" AND t.create_at >= datetime({repr(start_time)})"
+                              f" AND t.create_at < datetime({repr(end_time)})")
             logging.info(f"MODIFIED incremental sql: \n{sql}")
 
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
         data = {
             "statement": sql,
             "format": "application/x-adm"}
@@ -111,10 +135,10 @@ class PumpManager:
             'http': config['general']['proxy']
         } if config['general']['proxy'] else {}
 
-        r = requests.post(asterixdb_url, data=urllib.parse.urlencode(data), headers=headers, proxies=proxies)
-        if r.status_code != 200:
-            raise AsterixDBError(f"AsterixDB returned non 200 response: {r.status_code}, {r.content}")
-        return json.loads(r.content).get('results')
+        request = requests.post(asterixdb_url, data=urllib.parse.urlencode(data), headers=headers, proxies=proxies)
+        if request.status_code != 200:
+            raise AsterixDBError(f"AsterixDB returned non 200 response: {request.status_code}, {request.content}")
+        return json.loads(request.content).get('results')
 
     @staticmethod
     def persist_to_disk(data: List[str], out):
@@ -122,19 +146,15 @@ class PumpManager:
             for each in data:
                 file.write(each)
 
-    def push_to_asterixdb(self, data: List[str], host: str, port: int):
-
-        from socket import socket
-
-        sock1 = socket()
+    @staticmethod
+    def push_to_asterixdb(data: List[str], host: str, port: int):
+        _socket = socket()
         logging.info(f"Opening Feed on {host}:{port}")
-        sock1.connect((host, int(port)))
-
+        _socket.connect((host, int(port)))
         logging.info(f"Ingesting data through Feed opened on {host}:{port}")
-
         for each in data:
-            sock1.sendall(each.encode('utf-8'))
-        sock1.close()
+            _socket.sendall(each.encode('utf-8'))
+        _socket.close()
         logging.info(f"Done ingesting through Feed opened on {host}:{port}")
 
     def __str__(self) -> str:
@@ -154,13 +174,13 @@ class PumpManager:
 
 if __name__ == '__main__':
 
-    format = '[%(asctime)s] [%(levelname)s] %(funcName)s(): %(message)s'
+    log_format = '[%(asctime)s] [%(levelname)s] %(funcName)s(): %(message)s'
     if not os.path.exists(LOG_DIR):
         os.mkdir(LOG_DIR)
-    logging.basicConfig(format=format, level=logging.INFO, filename=RUNTIME_LOG_PATH, datefmt='%Y-%m-%dT%H:%M:%SZ')
+    logging.basicConfig(format=log_format, level=logging.INFO, filename=RUNTIME_LOG_PATH, datefmt='%Y-%m-%dT%H:%M:%SZ')
     if not os.path.exists(PERSISTENCE_DIR):
         os.mkdir(PERSISTENCE_DIR)
-    if eval(parse(PUMP_CONFIG_PATH)['general']['clean_history']):
+    if eval(parse(PUMP_CONFIG_PATH).get('general').get('clean_history')):
         os.remove(PUMP_MANAGER_PERSISTENCE_PATH)
     store = shelve.open(PUMP_MANAGER_PERSISTENCE_PATH)
     if 'pump_manager' not in store:
